dec5bfe2ab9f1ee32823e786d27fdf91dd080294
Navin540 <navinkumar.tem@gmail.com>
2022-12-10 23:30:58 -0800

    twofer

--- test/project_one_test.dart
+++ test/project_one_test.dart
@@ -1,12 +1,11 @@
 import 'package:project_one/resistor.dart';
+import 'package:project_one/two_for_one.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('color code test', () {
-    var c = Resistor();
-    expect(c.getColorCode('black'), 0);
-    expect(c.getColorCode('brown'), 1);
-    expect(c.getColorCode('red'), 2);
-    expect(c.getColorCode('white'), 9);
+  test('twofer', () {
+    var T = TwoForOne();
+    expect(T.twoFer('black'), 'one for black one for me');
+    expect(T.twoFer(''), 'one for you one for me');
   });
 }
